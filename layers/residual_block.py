2635abdc79cdaf2a993c028ba87c8377d86dfd96
jedlimlx <jedlimlx@outlook.com>
2022-08-26 14:35:20 +0800

    Complete U-Net implementation

--- layers/residual_block.py
+++ layers/residual_block.py
@@ -5,7 +5,7 @@ from layers.regularisation import StochasticDepth
 
 
 def ResidualBlock(filters, kernel_size=3, stride=1, conv_shortcut=True, name=None,
-                  activation="relu", drop_connect_rate=0.2):
+                  activation="relu", drop_connect_rate=0.2, dims=2):
     """A residual block.
     Args:
       x: input tensor.
@@ -17,27 +17,30 @@ def ResidualBlock(filters, kernel_size=3, stride=1, conv_shortcut=True, name=Non
       name: string, block label.
       activation: string, the activation function to use
       drop_connect_rate: string, the probability that the block will be skipped
+      dims: integer, the number of dimensions of the convolution (2d or 3d)
     Returns:
 
       Output tensor for the residual block.
     """
 
+    conv_block = Conv2D if dims == 2 else Conv3D
+
     def apply(x):
         if conv_shortcut:
-            shortcut = Conv2D(4 * filters, 1, strides=stride, use_bias=False, name=name + "_0_conv")(x)
+            shortcut = conv_block(4 * filters, 1, strides=stride, use_bias=False, name=name + "_0_conv")(x)
             shortcut = BatchNormalization(name=name + "_0_bn")(shortcut)
         else:
             shortcut = x
 
-        x = Conv2D(filters, 1, strides=stride, use_bias=False, name=name + "_1_conv")(x)
+        x = conv_block(filters, 1, strides=stride, use_bias=False, name=name + "_1_conv")(x)
         x = BatchNormalization(name=name + "_1_bn")(x)
         x = Activation(activation, name=name + f"_1_{activation}")(x)
 
-        x = Conv2D(filters, kernel_size, padding="SAME", use_bias=False, name=name + "_2_conv")(x)
+        x = conv_block(filters, kernel_size, padding="SAME", use_bias=False, name=name + "_2_conv")(x)
         x = BatchNormalization(name=name + "_2_bn")(x)
         x = Activation(activation, name=name + f"_2_{activation}")(x)
 
-        x = Conv2D(4 * filters, 1, use_bias=False, name=name + "_3_conv")(x)
+        x = conv_block(4 * filters, 1, use_bias=False, name=name + "_3_conv")(x)
         x = BatchNormalization(name=name + "_3_bn")(x)
 
         x = StochasticDepth(rate=drop_connect_rate, name=name + "_add")([shortcut, x])
@@ -47,7 +50,7 @@ def ResidualBlock(filters, kernel_size=3, stride=1, conv_shortcut=True, name=Non
     return apply
 
 
-def ResidualStack(filters, blocks, stride=2, name=None, activation="relu", drop_connect_rate=0.2):
+def ResidualStack(filters, blocks, name=None, kernel_size=3, activation="relu", drop_connect_rate=0.2, dims=2):
     """A set of stacked residual blocks.
     Args:
       filters: integer, filters of the bottleneck layer in a block.
@@ -56,16 +59,15 @@ def ResidualStack(filters, blocks, stride=2, name=None, activation="relu", drop_
       name: string, stack label.
       activation: string, the activation function to use
       drop_connect_rate: string, the probability that the block will be skipped
+      dims: integer, the number of dimensions of the convolution (2d or 3d)
     Returns:
       Output tensor for the stacked blocks.
     """
 
     def apply(x):
-        x = ResidualBlock(filters, stride=stride, name=name + "_block1", activation=activation,
-                          drop_connect_rate=drop_connect_rate)(x)
-        for i in range(2, blocks + 1):
+        for i in range(1, blocks + 1):
             x = ResidualBlock(filters, conv_shortcut=False, name=name + "_block" + str(i), activation=activation,
-                              drop_connect_rate=drop_connect_rate)(x)
+                              drop_connect_rate=drop_connect_rate, kernel_size=kernel_size, dims=dims)(x)
 
         return x
 
--- main.py
+++ main.py
@@ -1,14 +1,75 @@
 from tensorflow.keras.layers import *
 from tensorflow.keras.models import *
 
-from layers import ResidualStack
+from tensorflow.keras.utils import plot_model
 
-blocks = [6, 12, 24, 16, 12]
+from layers import ResidualStack, ResidualBlock
 
-input = Input(shape=(256, 256, 3))
-x = ResidualStack(64, blocks[0], name="conv1", activation="swish")(input)
-x = ResidualStack(64, blocks[1], name="conv2", activation="swish")(x)
+# hyperparameters
+ACTIVATION = "relu"
 
-model = Model(inputs=input, outputs=x)
-model.compile(optimizer="adam", loss="mse")
-model.summary()
+
+# the skip connections in the U-Net
+def skip_connection_2d_to_3d():
+    def apply(x):
+        x = Conv2D(x.shape[2], 3, activation=ACTIVATION, padding="same")(x)
+        x = Reshape((x.shape[1], x.shape[2], x.shape[2], 1))(x)
+        x = Conv3D(256, 3, activation=ACTIVATION, padding="same")(x)
+
+        return x
+
+    return apply
+
+
+# a stack of blocks + pooling (returns the final block too)
+def stack(filters, blocks, kernel_size=3, stride=2, name=None, activation="relu",
+          drop_connect_rate=0.2, dims=2, downsample=True):
+    def apply(x):
+        conv = ResidualStack(filters, blocks, name=name, activation=activation,
+                             drop_connect_rate=drop_connect_rate, kernel_size=kernel_size, dims=dims)(x)
+        if downsample:
+            # x = MaxPooling2D(2, name=name + "_pooling_block")(conv)
+            x = ResidualBlock(filters, stride=stride, name=name + "_pooling_block", activation=activation,
+                              drop_connect_rate=drop_connect_rate, kernel_size=kernel_size, dims=dims)(conv)
+        else:
+            x = UpSampling3D(size=(2, 2, 2), name=name + "_upsample")(conv)
+
+        return x, conv
+
+    return apply
+
+
+if __name__ == "__main__":
+    blocks = [2, 2, 2, 2, 2]
+
+    output_2d = []
+    output_3d = []
+    output_skip = []
+
+    inputs = Input(shape=(128, 128, 256))
+
+    # Downward 2D part of U-Net
+    for i in range(len(blocks)):
+        if i == 0:
+            x = inputs
+
+        x, conv = stack(64, blocks[i], name=f"stack_2d_{i}")(x)
+        output_2d.append(conv)
+        output_skip.append(skip_connection_2d_to_3d()(conv))
+
+    # Upward 3D part of U-Net
+    for i in range(len(blocks) - 1, -1, -1):
+        if i == len(blocks) - 1:
+            x = output_skip[i]
+        else:
+            x = Concatenate()([output_skip[i], x])
+            x = Conv3D(256, 3, activation=ACTIVATION, name=f"stack_3d_{i}_reshape", padding="same")(x)
+
+        x, conv = stack(64, blocks[i], dims=3, name=f"stack_3d_{i}", downsample=False)(x)
+        output_3d.append(conv)
+
+    model = Model(inputs=inputs, outputs=output_3d[-1])
+    model.compile(optimizer="adam", loss="mse")
+    model.summary()
+
+    plot_model(model)
