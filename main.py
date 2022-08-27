e1ee90e12b08f2c1110e4b0554da79e52169dd93
jedlimlx <jedlimlx@outlook.com>
2022-08-27 22:48:48 +0800

    Add code to read in the data

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+import random
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+import tensorflow as tf
+
 from tensorflow.keras.layers import *
 from tensorflow.keras.models import *
 
@@ -7,6 +14,7 @@ from layers import ResidualStack, ResidualBlock
 
 # hyperparameters
 ACTIVATION = "relu"
+BLOCKS = [2, 2, 2, 2, 2]
 
 
 # the skip connections in the U-Net
@@ -39,9 +47,7 @@ def stack(filters, blocks, kernel_size=3, stride=2, name=None, activation="relu"
     return apply
 
 
-if __name__ == "__main__":
-    blocks = [2, 2, 2, 2, 2]
-
+def create_model():
     output_2d = []
     output_3d = []
     output_skip = []
@@ -49,27 +55,48 @@ if __name__ == "__main__":
     inputs = Input(shape=(128, 128, 256))
 
     # Downward 2D part of U-Net
-    for i in range(len(blocks)):
+    for i in range(len(BLOCKS)):
         if i == 0:
             x = inputs
 
-        x, conv = stack(64, blocks[i], name=f"stack_2d_{i}")(x)
+        x, conv = stack(64, BLOCKS[i], name=f"stack_2d_{i}")(x)
         output_2d.append(conv)
         output_skip.append(skip_connection_2d_to_3d()(conv))
 
     # Upward 3D part of U-Net
-    for i in range(len(blocks) - 1, -1, -1):
-        if i == len(blocks) - 1:
+    for i in range(len(BLOCKS) - 1, -1, -1):
+        if i == len(BLOCKS) - 1:
             x = output_skip[i]
         else:
             x = Concatenate()([output_skip[i], x])
             x = Conv3D(256, 3, activation=ACTIVATION, name=f"stack_3d_{i}_reshape", padding="same")(x)
 
-        x, conv = stack(64, blocks[i], dims=3, name=f"stack_3d_{i}", downsample=False)(x)
+        x, conv = stack(64, BLOCKS[i], dims=3, name=f"stack_3d_{i}", downsample=False)(x)
         output_3d.append(conv)
 
     model = Model(inputs=inputs, outputs=output_3d[-1])
     model.compile(optimizer="adam", loss="mse")
     model.summary()
 
-    plot_model(model)
+    return model
+
+
+def plot_voxels(data1):
+    ax = plt.figure().add_subplot(projection='3d')
+    ax.voxels(data1)
+    plt.show()
+
+
+def data_generator(directory=""):
+    voxels_lst = []
+    detections_lst = []
+    for i in range(1000):
+        detections = []
+        for j in range(6):
+            detections.append(np.expand_dims(np.load(f"{directory}/detections/{i}_orient_{j}.npy"), axis=-1))
+
+        detections_lst.append(np.concatenate(detections, axis=-1))
+        voxels_lst.append(1 / (np.expand_dims(np.load(f"{directory}/voxels/run_{i}.npy") + 1e-8, axis=-1)))
+
+    return detections_lst, voxels_lst
+
