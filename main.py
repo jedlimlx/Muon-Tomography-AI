575c23a707cedc8c8b8be52ad59f394b399c89be
jedlimlx <jedlimlx@outlook.com>
2022-08-30 14:24:45 +0800

    Some fixes to the U-Net

--- main.py
+++ main.py
@@ -12,17 +12,13 @@ from tensorflow.keras.utils import plot_model
 
 from layers import ResidualStack, ResidualBlock
 
-# hyperparameters
-ACTIVATION = "relu"
-BLOCKS = [2, 2, 2, 2, 2]
-FILTERS = 64
 
 # the skip connections in the U-Net
-def skip_connection_2d_to_3d():
+def skip_connection_2d_to_3d(filters=64, activation="relu"):
     def apply(x):
-        x = Conv2D(x.shape[2], 3, activation=ACTIVATION, padding="same")(x)
+        x = Conv2D(x.shape[2], 3, activation=activation, padding="same")(x)
         x = Reshape((x.shape[1], x.shape[2], x.shape[2], 1))(x)
-        x = Conv3D(4 * FILTERS, 3, activation=ACTIVATION, padding="same")(x)
+        x = Conv3D(4 * filters, 3, activation=activation, padding="same")(x)
 
         return x
 
@@ -47,35 +43,39 @@ def stack(filters, blocks, kernel_size=3, stride=2, name=None, activation="relu"
     return apply
 
 
-def create_model(shape=(64, 64, 6)):
+def create_model(shape=(64, 64, 6), blocks=(2, 2, 2, 2, 2), filters=64, activation="relu",
+                 optimizer="adam", loss="binary_crossentropy", weights=None):
     output_2d = []
     output_3d = []
     output_skip = []
 
     inputs = Input(shape=shape)
-    x = Conv2D(4 * FILTERS, 3, padding="same", activation=ACTIVATION)(inputs)
+    x = Conv2D(4 * filters, 3, padding="same", activation=activation)(inputs)
 
     # Downward 2D part of U-Net
-    for i in range(len(BLOCKS)):
-        x, conv = stack(FILTERS, BLOCKS[i], name=f"stack_2d_{i}")(x)
+    for i in range(len(blocks)):
+        x, conv = stack(filters, blocks[i], name=f"stack_2d_{i}")(x)
         output_2d.append(conv)
-        output_skip.append(skip_connection_2d_to_3d()(conv))
+        output_skip.append(skip_connection_2d_to_3d(filters, activation)(conv))
 
     # Upward 3D part of U-Net
-    for i in range(len(BLOCKS) - 1, -1, -1):
-        if i == len(BLOCKS) - 1:
+    for i in range(len(blocks) - 1, -1, -1):
+        if i == len(blocks) - 1:
             x = output_skip[i]
         else:
             x = Concatenate()([output_skip[i], x])
-            x = Conv3D(4 * FILTERS, 3, activation=ACTIVATION, name=f"stack_3d_{i}_reshape", padding="same")(x)
+            x = Conv3D(4 * filters, 3, activation=activation, name=f"stack_3d_{i}_reshape", padding="same")(x)
 
-        x, conv = stack(FILTERS, BLOCKS[i], dims=3, name=f"stack_3d_{i}", downsample=False)(x)
+        x, conv = stack(filters, blocks[i], dims=3, name=f"stack_3d_{i}", downsample=False)(x)
         output_3d.append(conv)
 
-    model = Model(inputs=inputs, outputs=output_3d[-1])
-    model.compile(optimizer="adam", loss="mse")
+    outputs = Conv3D(64, 3, activation="sigmoid", padding="same", name="output")(output_3d[-1])
+
+    model = Model(inputs=inputs, outputs=outputs)
+    model.compile(optimizer=optimizer, loss=loss)
     model.summary()
 
+    model.load(weights)
     return model
 
 
@@ -97,7 +97,3 @@ def data_generator(directory=""):
         voxels_lst.append(np.expand_dims(np.load(f"{directory}/voxels/run_{i}.npy"), axis=-1))
 
     return detections_lst, voxels_lst
-
-
-if __name__ == "__main__":
-    model = create_model(shape=(64, 64, 6))
