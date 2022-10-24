ce10361cd5ef22930e358885e500a9d2d8f2ebdf
road-pig <qiuzeeming@gmail.com>
2022-10-24 19:38:27 +0800

    implement encoder decoder architecture

--- layers/vision_transformer.py
+++ layers/vision_transformer.py
@@ -4,6 +4,8 @@ import numpy as np
 from tensorflow.keras.layers import *
 from tensorflow.keras.models import *
 
+from functools import partial
+
 
 def get_angles(pos, i, d_model):
     angle_rates = 1 / np.power(10000, (2 * (i // 2)) / np.float32(d_model))
@@ -27,7 +29,18 @@ def positional_encoding(position, d_model):
 
 
 class MLP(Layer):
-    def __init__(self, hidden_dim, out_dim, dropout_rate, hidden_activation='gelu', out_activation='linear', name=None):
+    def __init__(self, hidden_dim, out_dim, dropout_rate=0.1,
+                 hidden_activation='gelu', out_activation='linear', name=None):
+        """
+        Represents the MLP used in vision transformers (2 dense layers)
+        Args:
+            hidden_dim: output dimensions of hidden layer
+            out_dim: output dimensions of output layer
+            dropout_rate: dropout rate (default 0.1)
+            hidden_activation: activation for hidden layer (default 'gelu')
+            out_activation: activation for output layer (default 'linear')
+            name: name of the layer
+        """
         super(MLP, self).__init__()
         self.dense1 = Dense(hidden_dim, activation=hidden_activation, name=f"{name}_dense_0")
         self.drop1 = Dropout(dropout_rate, name=f"{name}_dropout_0")
@@ -54,34 +67,22 @@ class MLP(Layer):
 #     return model
 
 
-def ViTBlock(num_heads, projection_dim, transformer_units, name=None):
-    def apply(encoded_patches):
-        # Layer normalisation 1
-        x1 = LayerNormalization(epsilon=1e-6, name=f"{name}_layer_norm_1")(encoded_patches)
+class ViTBlock(Layer):
+    def __init__(self, num_heads=16, dim=256, mlp_units=512, dropout=0.1, activation='gelu', name='vit_block',
+                 norm=partial(LayerNormalization, epsilon=1e-5), **kwargs):
+        super().__init__(name=name, **kwargs)
+        self.norm1 = norm(name=f"{name}_norm_1")
+        self.mhsa = MultiHeadAttention(num_heads=num_heads, key_dim=dim, dropout=dropout, name=f"{name}_mha")
+        self.norm2 = norm(name=f"{name}_norm_2")
+        self.mlp = MLP(hidden_dim=mlp_units, out_dim=dim, hidden_activation=activation, name=f"{name}_mlp")
 
-        # Multi-head attention
-        attention_output = MultiHeadAttention(
-            num_heads=num_heads,
-            key_dim=projection_dim,
-            dropout=0.1,
-            name=f"{name}_mhsa"
-        )(x1, x1)
-
-        # Skip connection 1
-        x2 = Add(name=f"{name}_add_1")([attention_output, encoded_patches])
-
-        # Layer Normalisation 2
-        x3 = LayerNormalization(epsilon=1e-6, name=f"{name}_layer_norm_2")(x2)
-
-        # Multi-layer perception
-        x3 = MLP(hidden_dim=transformer_units[0], out_dim=transformer_units[1],
-                 dropout_rate=0.1, name=f"{name}_mlp")(x3)
-
-        # Skip connection 2
-        encoded_patches = Add(name=f"{name}_add_2")([x3, x2])
-        return encoded_patches
-
-    return apply
+    def call(self, inputs, **kwargs):
+        x1 = self.norm1(inputs)
+        attn = self.mhsa(x1, x1)
+        x2 = attn + x1
+        x3 = self.norm2(x2)
+        x3 = self.mlp(x3)
+        return x2 + x3
 
 
 class Patches(Layer):
@@ -90,7 +91,7 @@ class Patches(Layer):
         self.patch_width = patch_width
         self.patch_height = patch_height
 
-    def call(self, images):
+    def call(self, images, **kwargs):
         batch_size = tf.shape(images)[0]
         patches = tf.image.extract_patches(
             images=images,
@@ -109,16 +110,16 @@ class PatchEncoder(Layer):
         super(PatchEncoder, self).__init__(name=name)
         self.num_patches = num_patches
         self.projection = Dense(units=projection_dim)
-        self.position_embedding = positional_encoding(num_patches, projection_dim)
+        self.position_embedding = Embedding(input_dim=num_patches, output_dim=projection_dim)
 
-    def call(self, patch):
+    def call(self, patch, **kwargs):
         positions = tf.range(start=0, limit=self.num_patches, delta=1)
-        encoded = self.projection(patch) + self.position_embedding
+        encoded = self.projection(patch) + self.position_embedding(positions)
         return encoded
 
 
 class PatchDecoder(Layer):
-    def __init__(self, patch_width, patch_height, x_patches, y_patches, projection_dim, mlp_units, name=None):
+    def __init__(self, patch_width, patch_height, x_patches, y_patches, name=None):
         super(PatchDecoder, self).__init__(name=name)
         self.patch_width = patch_width
         self.patch_height = patch_height
@@ -137,13 +138,13 @@ class PatchDecoder(Layer):
         #                        name=f"{name}_reshape_1")
         # self.concatenate = Concatenate(axis=-1)
 
-    def call(self, encoded):
+    def call(self, encoded, **kwargs):
         # positions = tf.range(start=0, limit=self.x_patches * self.y_patches, delta=1)
         # embedding = self.positional_embedding(positions)
 
         # Extracting patches
         # patches = self.mlp(encoded + self.positional_embedding)
-        reshaped = tf.reshape(encoded, (-1, 16, 16, 16, 16))
+        reshaped = tf.reshape(encoded, (-1, self.y_patches, self.x_patches, self.patch_height, self.patch_width))
         reshaped = tf.transpose(reshaped, [0, 1, 3, 2, 4])
         reshaped = tf.reshape(reshaped, (-1, 256, 256, 1))
 
@@ -154,3 +155,51 @@ class PatchDecoder(Layer):
         #     return x
 
         return reshaped
+
+
+def CTransformer(input_shape=(256, 256, 1), sinogram_height=1, sinogram_width=256, enc_dim=256, enc_layers=8,
+                 enc_mlp_units=512, enc_heads=16, dec_projection=True, dec_dim=256, dec_layers=8, dec_heads=16,
+                 dec_mlp_units=512, output_projection=True, output_patch_height=16, output_patch_width=16,
+                 output_x_patches=16, output_y_patches=16, dropout=0.1,
+                 activation='gelu', norm=partial(LayerNormalization, epsilon=1e-5)):
+    # error checking
+    if input_shape[0] % sinogram_height != 0 or input_shape[1] % sinogram_width != 0:
+        raise ValueError("Cannot divide image into even patches")
+
+    if (not dec_projection) and dec_dim != enc_dim:
+        raise ValueError("Encoder and decoder dims must match")
+
+    num_patches = int(input_shape[1] / sinogram_width * input_shape[0] / sinogram_height)
+
+    if (not output_projection) and dec_dim != output_patch_height * output_patch_width:
+        raise ValueError("Output patch size must patch decoder dims")
+
+    inputs = Input(shape=input_shape)
+
+    # patch
+    x = Patches(patch_height=sinogram_height, patch_width=sinogram_width, name="patchify")(inputs)
+    x = PatchEncoder(num_patches=num_patches, projection_dim=enc_dim, name="enc_projection")(x)
+
+    # encoder
+    for i in range(enc_layers):
+        x = ViTBlock(num_heads=enc_heads, mlp_units=enc_mlp_units, dropout=dropout, activation=activation, norm=norm,
+                     name=f"enc_block_{i}")(x)
+
+    # decoder projection
+    if dec_projection:
+        x = PatchEncoder(num_patches=num_patches, projection_dim=dec_dim, name="dec_projection")(x)
+
+    # decoder
+    for i in range(dec_layers):
+        x = ViTBlock(num_heads=dec_heads, mlp_units=dec_mlp_units, dropout=dropout, activation=activation, norm=norm,
+                     name=f"dec_block_{i}")(x)
+
+    # output projection
+    if output_projection:
+        x = PatchEncoder(num_patches=num_patches, projection_dim=output_patch_width * output_patch_height,
+                         name="output_projection")(x)
+
+    # reshape
+    x = PatchDecoder(output_patch_width, output_patch_height, output_x_patches, output_y_patches, name="depatchify")(x)
+
+    return Model(inputs=inputs, outputs=x)
