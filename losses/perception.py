4d225fc5171388d46aaa841e950f1e6e44ba38da
jedlimlx <jedlimlx@outlook.com>
2022-12-11 21:58:44 +0800

    Some changes to perception loss

--- losses/perception.py
+++ losses/perception.py
@@ -9,10 +9,10 @@ model = EfficientNetB3(
 )
 
 
-def perception_loss(y_pred, y_true, model=model):
+def perception_loss(y_pred, y_true, model=model, max_value=1.6):
     shape = tf.shape(y_pred)
-    y_pred = tf.reshape(y_pred, (shape[0] * shape[1], shape[2], shape[3], shape[4])) > 0.5
-    y_true = tf.reshape(y_true, (shape[0] * shape[1], shape[2], shape[3], shape[4]))
+    y_pred = tf.reshape(y_pred, (shape[0] * shape[1], shape[2], shape[3], shape[4])) / max_value * 255
+    y_true = tf.reshape(y_true, (shape[0] * shape[1], shape[2], shape[3], shape[4])) / max_value * 255
 
     feature_pred = model(tf.tile(y_pred, tf.constant([1, 1, 1, 3])))
     feature_true = model(tf.tile(y_true, tf.constant([1, 1, 1, 3])))
