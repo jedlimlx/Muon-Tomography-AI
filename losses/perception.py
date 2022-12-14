1161617ebd016a5f82a38f49118b574e108e8454
jedlimlx <jedlimlx@outlook.com>
2022-12-14 20:40:08 +0800

    Fixes to perception loss

--- losses/perception.py
+++ losses/perception.py
@@ -9,15 +9,15 @@ model = EfficientNetB3(
 )
 
 
-def perception_loss(y_pred, y_true, model=model, max_value=1.6):
+def perception_loss(y_pred, y_true, model, max_value=1.6):
     shape = tf.shape(y_pred)
-    y_pred = tf.reshape(y_pred, (shape[0] * shape[1], shape[2], shape[3], shape[4])) / max_value * 255
-    y_true = tf.reshape(y_true, (shape[0] * shape[1], shape[2], shape[3], shape[4])) / max_value * 255
+    y_pred = tf.math.divide(tf.reshape(y_pred, (shape[0] * shape[1], shape[2], shape[3], shape[4])), max_value / 255)
+    y_true = tf.math.divide(tf.reshape(y_true, (shape[0] * shape[1], shape[2], shape[3], shape[4])), max_value / 255)
 
     feature_pred = model(tf.tile(y_pred, tf.constant([1, 1, 1, 3])))
     feature_true = model(tf.tile(y_true, tf.constant([1, 1, 1, 3])))
 
-    mse = MeanSquaredError(reduction=tf.keras.losses.Reduction.SUM)
+    mse = tf.keras.losses.MeanSquaredError(reduction=tf.keras.losses.Reduction.SUM)
     return mse(feature_pred, feature_true)
 
 
