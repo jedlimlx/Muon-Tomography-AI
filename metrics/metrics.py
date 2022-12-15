e26fe2c7f5ebfdc0ab40ba0f77cb155860fe994b
road-pig <qiuzeeming@gmail.com>
2022-12-15 16:44:27 +0800

    change metrics to use subclass Metric

--- metrics/metrics.py
+++ metrics/metrics.py
@@ -1,28 +1,59 @@
 import tensorflow as tf
 
 
-def ssim(d_range=None):
-    if d_range is None:
-        d_range = lambda x: tf.reduce_max(x, axis=(1, 2), keepdims=True) - tf.reduce_min(x, axis=(1, 2), keepdims=True)
-    else:
-        d_range = lambda x: d_range
+class SSIM(tf.keras.metrics.Metric):
+    def __init__(self, d_range=None, rescaling=False, mean=None, std=None, **kwargs):
+        super().__init__(**kwargs)
+        self.shape = None
+        self.d_range = d_range
+        self.rescaling = rescaling
+        self.mean = mean
+        self.std = std
 
-    @tf.function
-    def apply(y_true, y_pred):
-        return tf.image.ssim(y_true, y_pred, d_range(y_true))
+        self.sum = self.add_weight('sum', initializer='zeros')
+        self.samples = self.add_weight('samples', initializer='zeros')
 
-    return apply
+    def update_state(self, y_true, y_pred, sample_weight=None):
+        if self.rescaling:
+            y_true = y_true * self.std + self.mean
+            y_pred = y_pred * self.std + self.mean
 
+        if self.d_range is None:
+            d_range = tf.reduce_max(y_true, axis=(1, 2), keepdims=True) - \
+                      tf.reduce_min(y_true, axis=(1, 2), keepdims=True)
+        else:
+            d_range = self.d_range
 
-def psnr(d_range=None):
-    if d_range is None:
-        # the way ssim and pnsr wants the d_range is slightly different
-        d_range = lambda x: tf.squeeze(tf.reduce_max(x, axis=(1, 2)) - tf.reduce_min(x, axis=(1, 2)))
-    else:
-        d_range = lambda x: d_range
+        self.sum.assign_add(tf.reduce_mean(tf.image.ssim(y_true, y_pred, d_range)))
+        self.samples.assign_add(1., tf.float32)
 
-    @tf.function
-    def apply(y_true, y_pred):
-        return tf.image.psnr(y_true, y_pred, d_range(y_true))
+    def result(self):
+        return self.sum / self.samples
 
-    return apply
+class PSNR(tf.keras.metrics.Metric):
+    def __init__(self, d_range=None, rescaling=False, mean=None, std=None, **kwargs):
+        super().__init__(**kwargs)
+        self.shape = None
+        self.d_range = d_range
+        self.rescaling = rescaling
+        self.mean = mean
+        self.std = std
+
+        self.sum = self.add_weight('sum', initializer='zeros')
+        self.samples = self.add_weight('samples', initializer='zeros')
+
+    def update_state(self, y_true, y_pred, sample_weight=None):
+        if self.rescaling:
+            y_true = y_true * self.std + self.mean
+            y_pred = y_pred * self.std + self.mean
+
+        if self.d_range is None:
+            d_range = tf.reshape(tf.reduce_max(y_true, axis=(1, 2)) - tf.reduce_min(y_true, axis=(1, 2)), (-1,))
+        else:
+            d_range = self.d_range
+
+        self.sum.assign_add(tf.reduce_mean(tf.image.psnr(y_true, y_pred, d_range)))
+        self.samples.assign_add(1., tf.float32)
+
+    def result(self):
+        return self.sum / self.samples
