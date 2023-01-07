011b8c3b137949c9775d5c025113309ba6a88235
road-pig <qiuzeeming@gmail.com>
2023-01-07 20:30:46 +0800

    r theta image

--- layers/diffusion.py
+++ layers/diffusion.py
@@ -3,10 +3,11 @@ from tensorflow.keras.models import *
 from tensorflow.keras.layers import *
 from functools import partial
 from layers.vision_transformer import Patches, PatchEncoder, DecoderBlock, PatchDecoder
+from layers.utils import Cart2Polar, Polar2Cart
 
 
-def DenoiseCT(mae, num_mask=0, dec_dim=256, dec_layers=8, dec_heads=16, dec_mlp_units=512, output_projection=False,
-              output_patch_height=16, output_patch_width=16, output_x_patches=16, output_y_patches=16,
+def DenoiseCT(mae, num_mask=0, dec_dim=256, dec_layers=8, dec_heads=16, dec_mlp_units=512, output_patch_height=16,
+              output_patch_width=16, output_x_patches=16, output_y_patches=16,
               norm=partial(LayerNormalization, epsilon=1e-5)):
     input_shape = mae.inp_shape
     num_patches = mae.num_patches
@@ -16,8 +17,6 @@ def DenoiseCT(mae, num_mask=0, dec_dim=256, dec_layers=8, dec_heads=16, dec_mlp_
                      output_patch_width * output_x_patches, 1), dtype='float32')]
 
     x, mask_indices, unmask_indices, y = inputs
-
-    print(y.shape)
 
     mae.patches.trainable = False
     x = mae.patches(x)
@@ -59,3 +58,55 @@ def DenoiseCT(mae, num_mask=0, dec_dim=256, dec_layers=8, dec_heads=16, dec_mlp_
 
     return Model(inputs=inputs, outputs=y)
 
+
+def CircleTransformer(mae, num_mask=0, dec_dim=256, dec_layers=8, dec_heads=16, dec_mlp_units=512,
+                      output_width=362, output_height=362, norm=partial(LayerNormalization, epsilon=1e-5)):
+    input_shape = mae.inp_shape
+    num_patches = mae.num_patches
+
+    inputs = [Input(input_shape), Input(num_mask, dtype=tf.int32), Input(num_patches - num_mask, dtype=tf.int32),
+              Input((output_height, output_width, 1), dtype='float32')]
+
+    x, mask_indices, unmask_indices, y = inputs
+
+    mae.patches.trainable = False
+    x = mae.patches(x)
+
+    mae.patch_encoder.trainable = False
+    mae.patch_encoder.num_mask = num_mask
+    (
+        unmasked_embeddings,
+        masked_embeddings,
+        unmasked_positions,
+        mask_indices,
+        unmask_indices,
+    ) = mae.patch_encoder(x, mask_indices, unmask_indices)
+
+    # Pass the unmaksed patche to the encoder.
+    encoder_outputs = unmasked_embeddings
+
+    for enc_block in mae.enc_blocks:
+        enc_block.trainable = False
+        encoder_outputs = enc_block(encoder_outputs)
+
+    mae.enc_norm.trainable = False
+    encoder_outputs = mae.enc_norm(encoder_outputs)
+
+    # Create the decoder inputs.
+    encoder_outputs = encoder_outputs + unmasked_positions
+    x = tf.concat([encoder_outputs, masked_embeddings], axis=1)
+
+    y = Cart2Polar(num_patches, dec_dim)(y)
+    y = tf.reshape(y, (-1, num_patches, dec_dim))
+    y = PatchEncoder(num_patches, dec_dim, embedding_type='learned', name='dec_projection')(y)
+
+    for i in range(dec_layers):
+        y = DecoderBlock(dec_heads, mae.enc_dim, dec_dim, mlp_units=dec_mlp_units, num_patches=num_patches,
+                         dropout=mae.dropout, activation=mae.activation, norm=norm, name=f'dec_block_{i}')((y, x))
+
+    y = norm(name='output_norm')(y)
+    y = Dense(dec_dim, name='output_projection')(y)
+    y = tf.expand_dims(y, -1)
+    y = Polar2Cart(output_height, output_width)(y)
+
+    return Model(inputs=inputs, outputs=y)
--- layers/utils.py
+++ layers/utils.py
@@ -1,4 +1,7 @@
 from tensorflow.keras.layers import *
+import tensorflow as tf
+import numpy as np
+import tensorflow_addons as tfa
 
 
 def skip_connection_2d_to_3d(filters, activation="relu", name=None):
@@ -20,3 +23,163 @@ def skip_connection_2d_to_3d(filters, activation="relu", name=None):
         return x
 
     return apply
+
+
+class Cart2Polar(Layer):
+    def __init__(self, angle_res=1024, r_res=256, name='cart2polar', **kwargs):
+        super().__init__(name=name, **kwargs)
+
+        self.angle_res = angle_res
+        self.r_res = r_res
+        self.coords = None
+
+    def build(self, input_shape):
+        n, h, w, c = input_shape
+        origin = tf.convert_to_tensor((w / 2, h / 2), dtype=tf.float32)
+
+        angles = tf.expand_dims(tf.linspace(-np.pi / 2, np.pi / 2, self.angle_res), -1)
+        r = tf.expand_dims(tf.linspace(-1., 1., self.r_res), 0)
+        r = tf.abs(r) * tf.sqrt(float(h * h + w * w)) / 2 * tf.sign(r)
+
+        x = r * tf.cos(angles)
+        y = r * tf.sin(angles)
+
+        coords = tf.stack([x, y], axis=-1) + origin
+        self.coords = tf.reshape(coords, (1, -1, 2))
+
+    def call(self, inputs, *args, **kwargs):
+        return tf.reshape(tfa.image.interpolate_bilinear(inputs, self.coords, indexing='xy'),
+                          (-1, self.angle_res, self.r_res, 1))
+
+    def get_config(self):
+        cfg = super(Cart2Polar, self).get_config()
+        cfg.update({
+            'angle_res': self.angle_res,
+            'r_res': self.r_res
+        })
+        return cfg
+
+
+def cart2polar(img, angle_res=1024, r_res=256, angle_range='180'):
+    assert angle_range in ['180', '360']
+    n, h, w, c = img.shape
+
+    # xy indexing because ij gets confusing very fast
+    origin = tf.convert_to_tensor((w / 2, h / 2), dtype=tf.float32)
+    if angle_range == '180':
+        angles = tf.expand_dims(tf.linspace(-np.pi / 2, np.pi / 2, angle_res), -1)
+        r = tf.expand_dims(tf.linspace(-1., 1., r_res), 0)
+        r = tf.abs(r) * tf.sqrt(float(h * h + w * w)) / 2 * tf.sign(r)
+    else:
+        angles = tf.expand_dims(tf.linspace(0., 2 * np.pi, angle_res), -1)
+        r = tf.expand_dims(tf.linspace(0., 1., r_res), 0)
+        r = tf.sqrt(r) * tf.sqrt(float(h * h + w * w)) / 2
+
+    x = r * tf.cos(angles)
+    y = r * tf.sin(angles)
+
+    coords = tf.stack([x, y], axis=-1) + origin
+    coords = tf.reshape(coords, (1, -1, 2))
+
+    return tf.reshape(tfa.image.interpolate_bilinear(img, coords, indexing='xy'), (-1, angle_res, r_res, 1))
+
+
+class Polar2Cart(Layer):
+    def __init__(self, h=362, w=362, name='polar2cart', **kwargs):
+        super().__init__(name=name, **kwargs)
+
+        self.h = h
+        self.w = w
+
+        self.coords = None
+
+    def build(self, input_shape):
+        n, angle_res, r_res, c = input_shape
+
+        x, y = tf.meshgrid(
+            tf.range(0, self.w, dtype=tf.float32),
+            tf.range(0, self.h, dtype=tf.float32)
+        )
+        origin = tf.convert_to_tensor((self.w / 2, self.h / 2), dtype=tf.float32)
+        x = x - origin[0]
+        y = y - origin[1]
+
+        r = tf.sqrt(x * x + y * y)
+        r = r / tf.sqrt(float(self.h * self.h + self.w * self.w))
+        th = tf.math.atan2(y, x)
+
+        r = r * tf.sign(tf.cos(th)) + 0.5
+        r = r * (r_res - 1)
+        th = tf.math.atan(tf.math.tan(th)) + np.pi / 2
+        th = th * (angle_res - 1) / np.pi
+
+        coords = tf.stack([th, r], axis=-1)
+        self.coords = tf.reshape(coords, (1, -1, 2))
+
+    def call(self, inputs, *args, **kwargs):
+        return tf.reshape(tfa.image.interpolate_bilinear(inputs, self.coords, indexing='ij'),
+                          (-1, self.h, self.w, 1))
+
+    def get_config(self):
+        cfg = super(Polar2Cart, self).get_config()
+        cfg.update({
+            'h': self.h,
+            'w': self.w
+        })
+        return cfg
+
+
+def polar2cart(img, h=512, w=512, angle_range='180'):
+    assert angle_range in ['180', '360']
+    n, angles_res, r_res, c = img.shape
+
+    # xy indexing because ij gets confusing very fast
+    x, y = tf.meshgrid(
+        tf.range(0, w, dtype=tf.float32),
+        tf.range(0, h, dtype=tf.float32)
+    )
+    origin = tf.convert_to_tensor((w / 2, h / 2), dtype=tf.float32)
+    x = x - origin[0]
+    y = y - origin[1]
+
+    if angle_range == '180':
+        r = tf.sqrt(x * x + y * y)
+        r = r / tf.sqrt(float(h * h + w * w))
+        th = tf.math.atan2(y, x)
+
+        r = r * tf.sign(tf.cos(th)) + 0.5
+        r = r * (r_res - 1)
+        th = tf.math.atan(tf.math.tan(th)) + np.pi / 2
+        th = th * (angles_res - 1) / np.pi
+    else:
+        r = x * x + y * y
+        r = r / (h * h + w * w) * 4 * r_res
+        th = tf.math.atan2(y, x) + np.pi
+        th = th * angles_res / np.pi
+
+    print(tf.reduce_sum(tf.cast(tf.math.is_inf(th), tf.int32)))
+    coords = tf.stack([th, r], axis=-1)
+    coords = tf.reshape(coords, (1, -1, 2))
+
+    return tf.reshape(tfa.image.interpolate_bilinear(img, coords, indexing='ij'), (-1, h, w, 1))
+
+
+def main():
+    import matplotlib.pyplot as plt
+    from data_generation import random_shapes
+    img = tf.reshape(next(random_shapes(1, 362, 362)), (1, 362, 362, 1))
+    plt.imshow(img[0], cmap='gray')
+    plt.show()
+
+    polar = Cart2Polar(angle_res=1024, r_res=512)(img)
+    print(polar[:, 0, 0, :])
+    plt.imshow(polar[0], cmap='gray')
+    plt.show()
+
+    # print(tf.reduce_sum(tf.cast(tf.math.is_nan(polar2cart(polar, h=256, w=256)), tf.int32)))
+    plt.imshow(Polar2Cart(h=362, w=362)(polar)[0], cmap='gray')
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
