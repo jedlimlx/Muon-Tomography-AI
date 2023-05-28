e2c78f2adfa4262b86014dfd8e6e6281e21653ec
jedlimlx <jedlimlx@outlook.com>
2023-05-28 22:17:49 +0800

    Fix reshape hopefully

--- layers/triplane.py
+++ layers/triplane.py
@@ -15,7 +15,7 @@ def Triplane(filters, kernel_size, padding="same", groups=1, name=None, activati
         def f(x, i):
             x = tf.reshape(x, (-1, shape[1], shape[2], shape[3] * shape[4]))
             x = Conv2D(filters, kernel_size, padding=padding, groups=groups, name=f"{name}_conv2d_{i}")(x)
-            x = tf.reshape(x, (-1,) + shape[1:])
+            x = tf.reshape(x, (-1, shape[1], shape[2], shape[3], shape[4]))
             return x
 
         shape = x.shape
