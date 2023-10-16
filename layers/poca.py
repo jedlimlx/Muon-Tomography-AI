cf07adfd5f0f01ad69ee7d306e34fa482f7443ab
jedlimlx <jedlimlx@outlook.com>
2023-10-16 21:58:38 +0800

    Fix some issues with the PoCA

--- layers/poca.py
+++ layers/poca.py
@@ -6,7 +6,7 @@ from tensorflow.keras.models import *
 from functools import partial
 
 
-def poca(x, p, ver_x, ver_p, threshold=1e-8, nn=None):
+def poca(x, p, ver_x, ver_p, threshold=1e-8, nn=None, not_scattered_mul=None):
     if nn is None:
         v = tf.linalg.cross(p, ver_p)
 
@@ -24,8 +24,15 @@ def poca(x, p, ver_x, ver_p, threshold=1e-8, nn=None):
 
         poca_points = (x + p * t + ver_x + ver_t * ver_p) / 2
 
-        k = tf.random.uniform(shape=tf.shape(scattered)[:-1], minval=0, maxval=1)
-        poca_points = poca_points * scattered + (x + (1 - k[..., tf.newaxis]) * ver_x) * not_scattered
+        if not_scattered_mul is None:
+            k = tf.random.uniform(shape=tf.shape(scattered)[:-1], minval=0, maxval=1)
+            poca_points = poca_points * scattered + (
+                    k[..., tf.newaxis] * x + (1 - k[..., tf.newaxis]) * ver_x
+            ) * not_scattered
+        else:
+            poca_points = poca_points * scattered + (
+                    not_scattered_mul * x + (1 - not_scattered_mul) * ver_x
+            ) * not_scattered
 
         return poca_points
     else:
