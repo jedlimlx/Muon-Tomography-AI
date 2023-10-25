Fix issues with random rotation

data_generation/data_augmentation.py
@@ -24,15 +24,17 @@ def random_flip(muons, voxels, extra_length=2):
 
 def random_rotate(muons, voxels):
     if random.randint(0, 1):
-        voxels = tf.transpose(voxels, (0, 2, 1, 3, 4))
+        voxels = tf.transpose(voxels, (0, 2, 1, 3))
         muons = tf.concat(
             [
                 muons[..., :2][::-1],
                 muons[..., 2:3],
-                muons[..., 3:6],
+                muons[..., 3:5][::-1],
+                muons[..., 5:6],
                 muons[..., 6:8][::-1],
                 muons[..., 8:9],
-                muons[..., 9:12],
+                muons[..., 9:11][::-1],
+                muons[..., 11:12],
                 muons[..., 12:],
             ], axis=1
         )