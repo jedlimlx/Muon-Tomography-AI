49f4cc6fc94e530170a20ccb96237e168fbcc417
jedlimlx <jedlimlx@outlook.com>
2023-11-28 07:19:51 +0900

    Finally fix data augmentation

--- data_generation/data_augmentation.py
+++ data_generation/data_augmentation.py
@@ -15,7 +15,7 @@ def random_flip(muons, voxels, extra_length=2):
     if vertical:
         voxels = voxels[:, ::-1, :, :]
         muons = (
-                tf.constant([1, -1, 1, -1, 1, 1, 1, -1, 1, -1, 1, 1] + [1] * extra_length, dtype=tf.float32) * muons +
+                tf.constant([1, -1, 1, 1, -1, 1, 1, -1, 1, 1, -1, 1] + [1] * extra_length, dtype=tf.float32) * muons +
                 tf.constant([0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0] + [0] * extra_length, dtype=tf.float32)
         )
 
@@ -27,15 +27,18 @@ def random_rotate(muons, voxels):
         voxels = tf.transpose(voxels, (0, 2, 1, 3))
         muons = tf.concat(
             [
-                muons[..., :2][::-1],
+                muons[..., 1:2],
+                muons[..., 0:1],
                 muons[..., 2:3],
-                muons[..., 3:5][::-1],
+                muons[..., 4:5],
+                muons[..., 3:4],
                 muons[..., 5:6],
-                muons[..., 6:8][::-1],
+                muons[..., 7:8],
+                muons[..., 6:7],
                 muons[..., 8:9],
-                muons[..., 9:11][::-1],
-                muons[..., 11:12],
-                muons[..., 12:],
+                muons[..., 10:11],
+                muons[..., 9:10],
+                muons[..., 11:]
             ], axis=-1
         )
 
